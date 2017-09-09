2d04f54142eb8b28dd9e60a001ab98d4c013b745
Conor Newton <conornewton@gmail.com>
2017-09-09 17:02:09 +0100

    vim plugin support included

--- install.sh
+++ install.sh
@@ -3,15 +3,24 @@
 #gets the path to the .dotfiles folder
 current_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-#Install vim symbolic link
-ln -sf $current_path/vim/vimrc ~/.vimrc
-
 #Install bashrc sym link
 ln -sf $current_path/bashrc/bashrc ~/.bashrc 
 
+#Install vim symbolic link
+ln -sf $current_path/vim/vimrc ~/.vimrc
 mkdir -p ~/.vim/colors
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/autoload
 
+#sym link colorscheme
 ln -sf $current_path/vim/colors/badwolf.vim ~/.vim/colors/badwolf.vim
+#sym link pathogen (plugin manager)
+ln -sf $current_path/vim/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
+
+#install vim plugins
+cd ~/.vim/bundle
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
+git clone https://github.com/idris-hackers/idris-vim.git
 
 #Install i3 dot files
 mkdir -p ~/.config/i3
