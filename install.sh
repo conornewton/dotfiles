b93f73671f4d6fa3f215ce9794ceff587521321a
Conor Newton <conornewton@gmail.com>
2017-12-05 16:54:52 +0000

    added screenshot support with scrot

--- install.sh
+++ install.sh
@@ -6,18 +6,15 @@ current_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #Install bashrc sym link
 ln -sf $current_path/bashrc/bashrc ~/.bashrc 
 
-#Install vim symbolic link
 ln -sf $current_path/vim/vimrc ~/.vimrc
 mkdir -p ~/.vim/colors
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.vim/autoload
 
-#sym link colorscheme
-ln -sf $current_path/vim/colors/badwolf.vim ~/.vim/colors/badwolf.vim
 #sym link pathogen (plugin manager)
 ln -sf $current_path/vim/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
 
-#install vim plugins
+#download vim plugins
 cd ~/.vim/bundle
 git clone https://github.com/vim-syntastic/syntastic.git
 git clone https://github.com/idris-hackers/idris-vim.git
