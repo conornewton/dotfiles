af17f542029b5c7288a2cbb69c50606383fa09d3
Conor Newton <conornewton@gmail.com>
2018-09-08 13:39:17 +0100

    fixed i3blocks sym links

--- i3blocks/install.sh
+++ i3blocks/install.sh
@@ -2,7 +2,7 @@ current_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 mkdir -p ~/.i3blocks/scripts
 
-ln -sf $current_path/i3blocks/i3blocks.conf ~/.i3blocks.conf
-ln -sf $current_path/i3blocks/scripts/battery.py ~/.i3blocks/scripts/battery.py
-ln -sf $current_path/i3blocks/scripts/volume-pulseaudio ~/.i3blocks/scripts/volume-pulseaudio
-ln -sf $current_path/i3blocks/scripts/wifi-wpa-supplicant.py ~/.i3blocks/scripts/wifi-wpa-supplicant.py
+ln -sf $current_path/i3blocks.conf ~/.i3blocks.conf
+ln -sf $current_path/scripts/battery.py ~/.i3blocks/scripts/battery.py
+ln -sf $current_path/scripts/volume-pulseaudio ~/.i3blocks/scripts/volume-pulseaudio
+ln -sf $current_path/scripts/wifi-wpa-supplicant.py ~/.i3blocks/scripts/wifi-wpa-supplicant.py
