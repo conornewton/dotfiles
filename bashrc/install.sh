c630dbc30b76b492404e51dbe22ff01225987db1
Conor Newton <conornewton@gmail.com>
2019-05-23 15:37:51 +0100

    removed old dotfiles added install for bashrc

---
+++ bashrc/install.sh
@@ -0,0 +1,2 @@
+current_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ln -sf $current_path/bashrc ~/.bashrc 
--- install.sh
+++ install.sh
@@ -2,12 +2,8 @@
 
 current_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-ln -sf $current_path/bashrc/bashrc ~/.bashrc 
-mkdir -p ~/.config/i3
-ln -sf $current_path/i3-gaps/config ~/.config/i3/config
-ln -sf $current_path/Xresources ~/.Xresources
+$current_path/bashrc/install.sh
 mkdir -p ~/.config/sway
 ln -sf $current_path/sway/config ~/.config/sway/config
-
 $current_path/i3blocks/install.sh
 $current_path/bin/install.sh
